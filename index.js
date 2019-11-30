9e638c1b943c333eb6a227995d96850cba694113
Mark <mark@remarkablemark.org>
2019-11-30 18:24:22 -0500

    feat(index): save users and repls json to `output/`

--- index.js
+++ index.js
@@ -4,23 +4,32 @@ const CommonPage = require('./pages/common');
 const HomePage = require('./pages/home');
 const UserPage = require('./pages/user');
 
+const { writeJson } = require('./util');
+
 (async function() {
   let driver;
 
   try {
     driver = await new Builder().forBrowser('chrome').build();
 
+    // go to home
     const common = new CommonPage(driver);
     await common.goToPage(HomePage.BASE_URL);
 
+    // get user links
     const userLinks = await common.getLinks(HomePage.USER_LINKS_LOCATOR);
-    console.log(userLinks);
+    await writeJson('users.json', userLinks);
+
+    // get repl links
+    let replLinks = [];
 
     for (let userLink of userLinks) {
       await common.goToPage(userLink);
-      const replLinks = await common.getLinks(UserPage.REPL_LINKS_LOCATOR);
-      console.log(replLinks);
+      const userReplLinks = await common.getLinks(UserPage.REPL_LINKS_LOCATOR);
+      replLinks = replLinks.concat(userReplLinks);
     }
+
+    await writeJson('repls.json', replLinks);
   } catch (err) {
     console.error(err);
   } finally {
